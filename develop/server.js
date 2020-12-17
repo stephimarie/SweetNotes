44e33b4ddf8dc43808905ae70a9aca50638d253e
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-16 20:05:46 -0800

    Add index.html

--- develop/server.js
+++ develop/server.js
@@ -16,3 +16,6 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./"))
+})
