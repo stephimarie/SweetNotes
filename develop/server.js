cf5c839f7c793f47d06ebc774e2183373526658d
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-16 20:00:52 -0800

    Add get request

--- develop/server.js
+++ develop/server.js
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "."))
-})
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
