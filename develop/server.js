f1f086e0c96b99b5742fe4805602728b2ce7e268
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-16 19:58:26 -0800

    Add public folder

--- develop/server.js
+++ develop/server.js
@@ -12,5 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "."))
+})
 
