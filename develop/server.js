791a9a2a85e3f39bec954db4862a602c0bfaf8d2
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-17 16:15:31 -0800

    Updated server.js

--- develop/server.js
+++ develop/server.js
@@ -23,3 +23,26 @@ app.get("/notes", function (req, res) {
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
+
+app.post("/api/notes", function (req, res) {
+    
+    var newNote= req.body;
+
+    var newId = uniqid();
+
+    newNote.id=newId;
+
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        let dbFile = JSON.parse(data);
+        dbFile.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(dbFile), "utf8", err => {
+            if (err) throw err;
+            console.log("The data was saved to file!");
+        });
+    });
+
+    res.redirect("/notes");
+
+});
